4d9668037597fbb8cfbe54ae5f60c3ecc5c5140d
GerganaRoeva <gergana.roeva@gmail.com>
2024-12-10 16:51:03 +0200

    base for landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import LandingPage from './app/LandingPage/LandingPage';
 import NavBar from './components/NavBar/NavBar';
+
 // import test from './components/test/test'
 
 function App() {
   return (
     <div className="App">
      <NavBar></NavBar>
+     <LandingPage></LandingPage>
     </div>
   );
 }

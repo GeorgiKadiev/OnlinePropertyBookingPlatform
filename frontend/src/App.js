21adc386edbf2cf9002dde4e0f6721485c295ddb
GerganaRoeva <gergana.roeva@gmail.com>
2024-12-22 01:37:37 +0200

    routing base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
-// import LandingPage from './app/LandingPage/LandingPage';
-// import ResultsPage from './app/ResultsPage/ResultsPage';
-// import NavBar from './components/NavBar/NavBar';
-// import LoginForm from './components/LoginForm/LoginForm';
-// import RegisterForm from './components/RegisterForm/RegisterForm';
+import LandingPage from './app/LandingPage/LandingPage';
+import ResultsPage from './app/ResultsPage/ResultsPage';
+import NavBar from './components/NavBar/NavBar';
+import LoginForm from './components/LoginForm/LoginForm';
+import RegisterForm from './components/RegisterForm/RegisterForm';
 import { Routes, Route } from "react-router-dom";
-
-
-// import test from './components/test/test'
+import HomePage from './app/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-     {/* <NavBar></NavBar> */}
-     <RegisterForm/>
-     {/* <LoginForm/> */}
-     {/* <LandingPage></LandingPage> */}
-     {/* <ResultsPage/> */}
+     <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+      </Routes>
     </div>
   );
 }

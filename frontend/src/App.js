8f2a9612b52f2e18644fa83da7b6161d7643681e
GerganaRoeva <gergana.roeva@gmail.com>
2024-12-22 00:39:58 +0200

    update afer pull mistace

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import LandingPage from './app/LandingPage/LandingPage';
-import ResultsPage from './app/ResultsPage/ResultsPage';
-import NavBar from './components/NavBar/NavBar';
-import LoginForm from './components/LoginForm/LoginForm';
-import RegisterForm from './components/RegisterForm/RegisterForm';
+// import LandingPage from './app/LandingPage/LandingPage';
+// import ResultsPage from './app/ResultsPage/ResultsPage';
+// import NavBar from './components/NavBar/NavBar';
+// import LoginForm from './components/LoginForm/LoginForm';
+// import RegisterForm from './components/RegisterForm/RegisterForm';
+import { Routes, Route } from "react-router-dom";
+
 
 // import test from './components/test/test'
 
@@ -11,8 +13,8 @@ function App() {
   return (
     <div className="App">
      {/* <NavBar></NavBar> */}
-     {/* <RegisterForm/> */}
-     <LoginForm/>
+     <RegisterForm/>
+     {/* <LoginForm/> */}
      {/* <LandingPage></LandingPage> */}
      {/* <ResultsPage/> */}
     </div>
